b66f55aafb0537ef406211760b2cb1c132010946
syarifh-id <125syarifh@gmail.com>
2024-08-02 20:22:32 +0700

    parameter on function

---
+++ c/hello.c
@@ -0,0 +1,20 @@
+
+int main() {
+
+  // 1 paramaeter 
+  printf("Hello World!");
+  return 0;
+
+  //2 parameter
+  char name[] = "John";
+  printf("Hello, %s", name);
+
+  //3 parameter
+  char firstName[] = "John";
+  char lastName[] = "doe";
+  printf("Hello, %s, %s", firstName, lastName);
+
+}
+
+
+
